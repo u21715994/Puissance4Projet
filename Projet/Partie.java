f8d82cfea2315dcef0777ac7f86f4a164615539f
Youssef Oussouf <youssef.oussouf@etu.u-pec.fr>
2020-12-07 18:20:50 +0100

    Class Partie

---
+++ Projet/Partie.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Partie{
+    public static void main(String [] args){
+        int partie = 1;
+        while(partie == 1){
+            Scanner clavier = new Scanner(System.in);
+            System.out.println("Bienvenue dans Puissance 4");
+            System.out.println();
+            System.out.println("Veuillez sélectionnez un mode de jeu");
+            System.out.println("1-Joueur contre Joueur  |  2-Joueur contre Ordinateur");
+            int p = clavier.nextInt();
+            while(p != 1 && p != 2){
+                if(p == 1)
+                    JvsJ.main(args);
+                    else
+                    JvsIA.main(args);
+            }
+            System.out.println();
+            System.out.println("Voulez-vous quitter Puissance 4");
+            System.out.println("0-Oui  |  1-Non");
+            partie = clavier.nextInt();
+            while(partie != 1 && partie != 0){
+                System.out.println("Erreur.Veuillez entrer les bonnes valeurs");
+                partie = clavier.nextInt();
+            }
+        }
+        System.out.println("Fin de Puissance 4");
+    }
+}
